c13ac68042dfdcd7abfc60144222cbe2d9452056
shihuichao <shihuichao@gitv.cn>
2023-03-09 16:20:12 +0800

    Test

---
+++ src/main/java/com/example/demo/controller/TestCtrl.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+@Slf4j
+public class TestCtrl {
+
+    @ResponseBody
+    @RequestMapping("hello")
+    public String Rtvodchndata() {
+        return "Hello World!";
+    }
+}
